4d4ff6d3eeaaad9a0a55281e01d79fd75da918d6
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2018-07-05 19:33:59 +0200

    Dodanie metod odpowiedzialnych za edycje postow

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -11,7 +11,7 @@
     <?php if($data['post']->user_id == $_SESSION['user_id']) : ?>
 
     <hr>
-    <a href="<?php URLROOT; ?>/posts/edit/<?php echo $data['post']->id; ?>" class="btn btn-dark">Edit</a>
+    <a href="<?php echo URLROOT; ?>/posts/edit/<?php echo $data['post']->id; ?>" class="btn btn-dark">Edit</a>
 
     <form class="float-right" action="<?php echo URLROOT; ?>/posts/delete/<?php echo $data['post']->id; ?>" method="post">
         <input type="submit" value="Delete" class="btn btn-danger">
